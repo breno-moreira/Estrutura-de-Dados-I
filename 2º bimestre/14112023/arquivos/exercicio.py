82ef7b1aaf04cc9bff58ee8268f618b1f4bcdb3d
breno-moreira <129233688+breno-moreira@users.noreply.github.com>
2023-11-28 21:12:14 -0300

    Update exercicio.py

--- 2º bimestre/14112023/arquivos/exercicio.py
+++ 2º bimestre/14112023/arquivos/exercicio.py
@@ -1,11 +1,11 @@
-""" with open ('C:\\Users\\teixe\\OneDrive\\Documentos\\FACULDADE\\2º semestre\\Estrutura de Dados I\\2º bimestre\\14112023\\arquivos\\entrada.txt', 'r') as arquivo:
+""" with open ('2º bimestre/14112023/arquivos/entrada.txt', 'r') as arquivo:
     num1 = int(arquivo.readline())
     num2 = int(arquivo.readline())
 
 soma = num1 + num2
 print(soma) """
 
-arquivo = "C:\\Users\\teixe\\OneDrive\\Documentos\\FACULDADE\\2º semestre\\Estrutura de Dados I\\2º bimestre\\14112023\\arquivos\\entrada.txt"
+arquivo = "2º bimestre/14112023/arquivos/entrada.txt"
 
 def leitura(nome_arquivo: str):
     try:
